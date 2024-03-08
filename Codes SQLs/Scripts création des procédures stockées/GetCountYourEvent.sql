571199219b84e59bafe0d966a9f2f1818f943956
ATOUGAEmmanuelDesire <162590546+ATOUGAEmmanuelDesire@users.noreply.github.com>
2024-03-09 00:12:09 +0100

    Trouver les éléments pour les notification
    Compter les évènements valider pour un utilisateur

---
+++ Codes SQLs/Scripts création des procédures stockées/GetCountYourEvent.sql
@@ -0,0 +1,29 @@
+/*PROJET WEB
+Promotion X2027
+    GROUPE 05 :
+    -ATOUGA II Emmanuel Désiré
+    -JODOM CHOUDJA Karell Steve
+    -ANDOH Sus-Shinley Mbaayi
+    -FOTSO Emmanuel Jordan
+    -TSAJIO Fils Logan
+    -TIAM DJOFANG Rita Joyce Emmanuela*/
+
+/* Activation de la base de données Cesi_BDE */
+USE Cesi_BDE;
+
+
+/*Création de la procédure permettant de trouver les compter évènements de l'utilisateur qui ont été validés*/
+
+DELIMITER //
+
+CREATE PROCEDURE GetCountYourEvent(
+    IN p_UserID INT,
+    OUT count INT
+)
+BEGIN
+    SELECT COUNT(*) INTO count
+    FROM Events
+    WHERE statut = 1 AND id_Users = p_UserID;
+END //
+
+DELIMITER ;

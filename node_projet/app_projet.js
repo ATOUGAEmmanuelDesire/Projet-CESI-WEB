beb4c5f12d60034f4936741ecb98c698bfe3c289
logan496 <fils.tsajio@2027.ucac-icam.com>
2024-03-10 11:30:18 +0100

    premiére implementations d'ejs

--- node_projet/app_projet.js
+++ node_projet/app_projet.js
@@ -1,33 +1,37 @@
-const express = require('express');
-
-const mysql = require('mysql');
-const myConnection = require('express-myconnection');
-
-const optionbd = {
-    host : 'localhost',
-    user : 'root',
-    password : 'inconnu_X2027',
-    port : '3306',
-    database : 'cesi_bde'
-};
-
+const express = require('express')
 const app = express();
 
-app.get('/cesi-bde/accueil', (req, res)=>{
-    res.status(200).sendFile('fronTest/accueil.html', {root : __dirname});
+app.set('view engine', 'ejs')
+app.set('views', 'frontTest')
+
+app.use(express.static('public'));
+
+app.get('/cesi-bde/accueil', (req, res, next)=>{
+    res.status(200).render('accueil');
 })
 
-app.get('/cesi-bde/inscription', (req, res)=>{
-    res.status(200).sendFile('frontTest/inscription.php', {root: __dirname});
+app.get('/cesi-bde/inscription', (req, res, next)=>{
+    res.status(200).render('inscription')
 })
-app.get('/cesi-bde/connexion', (req, res)=>{
-    res.status(200).sendFile('frontTest/connexion.php', {root: __dirname});
+
+app.get('/cesi-bde/connexion', (req, res, next)=>{
+    res.status(200).render('connexion')
 })
-app.get('/cesi-bde/social', (req, res)=>{
-    res.status(200).sendFile('fronTest/social.html', {root : __dirname});
+
+app.get('/cesi-bde/social', (req, res, next)=>{
+    res.status(200).render('social')
 })
-app.get('/cesi-bde/boutique', (req, res)=>{
-    res.status(200).sendFile('frontTest/boutique.htm', {root: __dirname});
+
+app.get('/cesi-bde/boutique', (req, res, next)=>{
+    res.status(200).render('boutique')
 })
+
+app.get('/cesi-bde/events', (req, res, next)=>{
+    res.status(200).render('events')
+})
+app.use('/', (req, res)=>{
+    res.status(404).render('notfound')
+})
+
 
 app.listen(3000);
